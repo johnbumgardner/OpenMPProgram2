3a82c55acfe89c98387b8a2b4603947f5fa2eb92
John Bumgardner <john.bumgardner@icloud.com>
2021-06-15 16:39:49 -0400

    add for j loop paralellization

--- openmp/pi_openmp.c
+++ openmp/pi_openmp.c
@@ -27,13 +27,13 @@ int main(int argc, char*argv[]){
         return -1;
     }
 
+    // Parallelize iterations of the for-i loop
+
     int circleCount = 0;
     double interval = 0, pi = 0;
     int i = 0, j = 0;
 
     interval = 1.0/(double)numPartitions;
-    
-    // Parallelize iterations of the for-i loop
 
     #pragma omp parallel for default(shared) private(i) 
     {
@@ -55,6 +55,38 @@ int main(int argc, char*argv[]){
     
     pi = (double)(4*circleCount)/(numPartitions * numPartitions);
     printf("For-i parallelization of estimate of pi is: %10.8lf\n", pi);
+
+    // Parallelize iterations of the for-j loop, one iteration of the for i loop at a time
+
+    int circleCount = 0;
+    double interval = 0, pi = 0;
+    int i = 0, j = 0;
+
+    interval = 1.0/(double)numPartitions;
+    
+    // Parallelize iterations of the for-i loop
+
+    
+    for (i = 0; i < numPartitions; i++) 
+    {
+        double a = (i + .5)*interval;
+        #pragma omp parallel for default(shared) private(j) 
+        {
+            for (j = 0; j < numPartitions; j++) 
+            {
+                double b = (j + .5)*interval;
+                if ((a*a + b*b) <= 1) 
+                {
+                    circleCount++;
+                }
+            }
+        } 
+    }
+    
+    
+    
+    pi = (double)(4*circleCount)/(numPartitions * numPartitions);
+    printf("For-j with one i iteration done at a time parallelization of estimate of pi is: %10.8lf\n", pi);
 
     return 0;
 
