4712d74a345cda460008f9e3e62b1c37f70933cf
John Bumgardner <john.bumgardner@icloud.com>
2021-06-15 17:18:07 -0400

    add final task for openmp

--- openmp/pi_openmp.c
+++ openmp/pi_openmp.c
@@ -63,10 +63,7 @@ int main(int argc, char*argv[]){
     i = 0, j = 0;
 
     interval = 1.0/(double)numPartitions;
-    
-    // Parallelize iterations of the for-i loop
 
-    
     for (i = 0; i < numPartitions; i++) 
     {
         double a = (i + .5)*interval;
@@ -87,6 +84,56 @@ int main(int argc, char*argv[]){
     
     pi = (double)(4*circleCount)/(numPartitions * numPartitions);
     printf("For-j with one i iteration done at a time parallelization of estimate of pi is: %10.8lf\n", pi);
+
+    // Parallelize iterations of the for-j loop, without respect to whether they are in iteration of i
+
+    circleCount = 0;
+    interval = 0, pi = 0;
+    i = 0, j = 0;
+
+
+
+    interval = 1.0/(double)numPartitions;
+
+    // write parallel to array that stores a and b
+    
+    double a[numPartitions];
+    double b[numPartitions];
+
+    int k = 0;
+    #pragma omp parallel for default(shared) private(k) 
+    {
+        for (k = 0; k < numPartitions; k++) 
+        {
+            a[k] = (k + .5) * interval;
+            b[k] = (k + .5) * interval;
+        }    
+    } 
+
+    #pragma omp parallel for default(shared) private(i) 
+    {
+        for (i = 0; i < numPartitions; i++) 
+        {
+            #pragma omp parallel for default(shared) private(j) 
+            {
+                for (j = 0; j < numPartitions; j++) 
+                {
+                    if ((a[i]*a[i] + b[j]*b[j]) <= 1) 
+                    {
+                        circleCount++;
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+    
+    
+    
+    pi = (double)(4*circleCount)/(numPartitions * numPartitions);
+    printf("For-j without regard for i iteration done at a time parallelization of estimate of pi is: %10.8lf\n", pi);
 
     return 0;
 
